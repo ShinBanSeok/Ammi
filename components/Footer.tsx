c05e2096fc188b753f3d091b6dfb94071e49ab5e
sbs <iop000126@naver.com>
2025-09-03 02:32:33 +0900

    footer수정

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,18 +7,31 @@ export default function Footer() {
   return (
     <footer className="my-3 md:my-0 bg-white border-t border-black/10">
       <Container>
-        <div className="flex flex-wrap items-center py-4 md:py-8 text-xs md:text-sm leading-5 md:leading-6 text-gray-600">
-          <div className="pr-3 md:pr-6 mb-1 md:mb-0">회사명: {companyName}</div>
-          <div className="border-r border-gray-400 h-3 hidden md:block"></div>
-          <div className="px-3 md:px-6 mb-1 md:mb-0">대표자: {ceo}</div>
-          <div className="border-r border-gray-400 h-3 hidden md:block"></div>
-          <div className="px-0 md:px-6 mb-1 md:mb-0">주소: {address.main}</div>
-          <div className="border-r border-gray-400 h-3 hidden md:block"></div>
-          <div className="px-0 md:px-6 mb-1 md:mb-0">전화번호: {phone}</div>
-          <div className="border-r border-gray-400 h-3 hidden md:block"></div>
-          <div className="pl-3 md:pl-6 mb-1 md:mb-0">
-            사업자등록번호: {bizRegNo}
+        {/* 모바일 전용 3줄 레이아웃 */}
+        <div className="block md:hidden py-4 text-xs leading-5 text-gray-600 space-y-2">
+          <div className="flex">
+            <span className="pr-3">회사명: {companyName}</span>
+            <span className="px-3">대표자: {ceo}</span>
           </div>
+          <div className="px-3">주소: {address.main}</div>
+          <div className="flex">
+            <span className="pr-3">전화번호: {phone}</span>
+            <span className="px-3">사업자등록번호: {bizRegNo}</span>
+          </div>
+          <div className="mt-4 text-gray-500 text-xs px-3">{copyright}</div>
+        </div>
+
+        {/* 데스크톱 기존 레이아웃 */}
+        <div className="hidden md:flex flex-wrap items-center py-8 text-sm leading-6 text-gray-600">
+          <div className="pr-6">회사명: {companyName}</div>
+          <div className="border-r border-gray-400 h-3"></div>
+          <div className="px-6">대표자: {ceo}</div>
+          <div className="border-r border-gray-400 h-3"></div>
+          <div className="px-6">주소: {address.main}</div>
+          <div className="border-r border-gray-400 h-3"></div>
+          <div className="px-6">전화번호: {phone}</div>
+          <div className="border-r border-gray-400 h-3"></div>
+          <div className="pl-6">사업자등록번호: {bizRegNo}</div>
           <div className="w-full mt-2 text-gray-500 text-xs">{copyright}</div>
         </div>
       </Container>
