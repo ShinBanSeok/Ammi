44fe6b66a664aa53bca78168a987d07fff2a12bf
sbs <iop000126@naver.com>
2025-09-02 22:02:08 +0900

    네이버metadata추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,9 @@ import Footer from '@/components/Footer';
 export const metadata: Metadata = {
   title: '(주)암미',
   description: 'Modern corporate website (Next.js + TypeScript)',
+  other: {
+    'naver-site-verification': '19c31a55dd6e7636a32bc812f5bd53d2f',
+  },
 };
 
 export default function RootLayout({
